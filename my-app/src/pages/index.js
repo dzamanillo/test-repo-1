9c40703a0b4ec0baedb128c66fe731b77a83ffa0
Daniel Zamanillo <dzamanillok@gmail.com>
2023-02-22 10:53:01 -0500

    test

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -24,6 +24,7 @@ export default function Home() {
 			<main className={styles.main}>
 				<h1>This is Test-Repo-1</h1>
 				<p>This is the stage branch</p>
+				<p>Sup</p>
 				{variable === "true" ? <p>It is true</p> : <p>It is false</p>}
 			</main>
 		</>
